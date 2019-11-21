0774fa2b2dbcd804e6163864d9fd93fa4bdb7caa
gilbert.mendoza <gilbert.mendoza@appetiser.com.au>
2019-11-21 13:22:38 +0800

    Remove default value from enum Anchor

--- Source/Extensions/UIView+Layout.swift
+++ Source/Extensions/UIView+Layout.swift
@@ -10,10 +10,10 @@ import UIKit
 
 @available(iOS 9.0, *)
 public enum Anchor {
-    case top(_ top: NSLayoutYAxisAnchor, constant: CGFloat = 0)
-    case leading(_ leading: NSLayoutXAxisAnchor, constant: CGFloat = 0)
-    case bottom(_ bottom: NSLayoutYAxisAnchor, constant: CGFloat = 0)
-    case trailing(_ trailing: NSLayoutXAxisAnchor, constant: CGFloat = 0)
+    case top(_ top: NSLayoutYAxisAnchor, constant: CGFloat)
+    case leading(_ leading: NSLayoutXAxisAnchor, constant: CGFloat)
+    case bottom(_ bottom: NSLayoutYAxisAnchor, constant: CGFloat)
+    case trailing(_ trailing: NSLayoutXAxisAnchor, constant: CGFloat)
     case height(_ constant: CGFloat)
     case width(_ constant: CGFloat)
 }
